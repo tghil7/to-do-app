d344fec447c7f1ecbc037311d4d1873a5e001954
Anicet <anicet@bdesmarest.onmicrosoft.com>
2024-09-28 21:55:35 -0500

    First push

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def get_todos(filepath):
     with open(filepath, 'r') as file:
-        my_todos = file.readline()
+        my_todos = file.readlines()
     return my_todos
 
 
 def write_todos(filepath, new_task):
-    with open(filepath, "w") as file:
+    with open(filepath, "a") as file:
+        file.write('\n')
         file.writelines(new_task)
 
 
@@ -15,10 +16,8 @@ while True:
     user_action = user_action.strip()
     if user_action.startswith("add"):
         # Read user's actions starting at index 4
-        todo = user_action[4:]
-        todos = get_todos('todos.txt')
-        todos.join('\n' + todo + '\n')
-        write_todos("todos.txt", todos)
+        todo = [user_action[4:]]
+        write_todos('todos.txt', todo)
 
     elif user_action.startswith("show"):
         # Read user's actions starting at index 5
@@ -27,15 +26,18 @@ while True:
         for line in todos:
             # Print the line
             print(line, end='')
+        print('\n')
 
     elif user_action.startswith('edit'):
         try:
             # Read user's actions starting at index 4
             number = int(user_action[5:])
             print(number)
-            number -= 1
             todos = get_todos("todos.txt")
             new_todo = input("Enter new todo:")
+            print('Number to remove:{0}'.format(todos[number]))
+            todos.remove(todos[number])
+            todos.insert(number, new_todo)
             write_todos("todos.txt", new_todo)
         except ValueError:
             print("Your command is not valid.")
@@ -48,7 +50,7 @@ while True:
             row = f"{index + 1}-{item}"
             print(row)
 
-    elif user_action.startswith("Exit"):
+    elif user_action.startswith("exit"):
         exit(0)
 
 
